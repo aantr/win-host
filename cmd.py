680b5f82b47a18066c298f70f3b7f4849845bba1
aantr <66200011+aantr@users.noreply.github.com>
2021-01-30 22:16:50 +0500

    Add files via upload

--- cmd.py
+++ cmd.py
@@ -140,7 +140,7 @@ def cmd(s: str, directory='', v=False):
     # ?
     variables = {'im': os.path.join(directory, 'images'),
                  'path': current_path}
-    for k, v in variables:
+    for k, v in variables.items():
         s = s.replace(f'?{k}', v)
 
     if s[0] == '.':
